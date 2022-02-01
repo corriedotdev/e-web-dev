81157f7c70be28c4e212f898ba22bb4949763943
Corrie Green <corriejgreen@users.noreply.github.com>
2022-02-01 09:04:55 +0000

    make it better

--- file.js
+++ file.js
@@ -1,11 +1,15 @@
-var http = require("http");
-http.createServer(function (request, response) {
- // Send the HTTP header
- // HTTP Status: 200 : OK
- // Content Type: text/plain
- response.writeHead(200, {'Content-Type': 'text/plain'});
- // Send the response body as "Hello World"
- response.end('Hello World\n');
-}).listen(8081);
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+///////////////////////////////////////////////////////////////////
+// load the express package and create our app
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 8080;
+// set the port based on environment (more on environments later)
+var port = PORT;
+// send our index.html file to the user for the home page
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/index.html');
+});
+// start the server
+app.listen(PORT);
+console.log('Express Server running at http://127.0.0.1:'.PORT);
+///////////////////////////////////////////////////////////////////
